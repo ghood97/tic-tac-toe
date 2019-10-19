925c1264b74d3b48498dd0abf4594f2eb75948dd
Griffin Hood <griffinhood@verizon.net>
2019-10-19 16:56:40 -0400

    Update ReadMe

--- assets/scripts/game/api.js
+++ assets/scripts/game/api.js
@@ -15,7 +15,8 @@ const index = () => {
 }
 
 const lookupGame = (id) => {
-  return $.ajax({url: config.apiUrl + `/games/${id}`,
+  return $.ajax({
+    url: config.apiUrl + `/games/${id}`,
     method: 'GET',
     headers: {
       Authorization: 'Token token=' + store.user.token
